11300523a0968fd02e21595602a7d17dbc4e5aaa
Jan Engelmohr <jan.engelmohr@mailbox.tu-dresden.de>
2016-07-28 16:06:30 +0200

    try to build own lcm_list unfortunately this leads to linker errors. agold_lcm_power_down and agold_lcm_power_up are needed and they seemingly are only available in the prebuilts...

--- drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
+++ drivers/misc/mediatek/lcm/mt65xx_lcm_list.h
@@ -249,6 +249,11 @@ extern LCM_DRIVER hx8394c_wxga_dsi_vdo_lcm_drv;
 extern LCM_DRIVER nt35595_truly_fhd_dsi_vdo_lcm_drv;
 extern LCM_DRIVER nt36850_wqhd_dsi_2k_cmd_lcm_drv;
 extern LCM_DRIVER s6e3ha3_wqhd_2k_cmd_lcm_drv;
+extern LCM_DRIVER HX8399_C_CSHMDS11487QR_lcm_drv;
+extern LCM_DRIVER TDO_BD0546U71067_lcm_drv;
+extern LCM_DRIVER H546DAN08_lcm_drv;
+extern LCM_DRIVER TDO_BD0546U71070_lcm_drv;
+extern LCM_DRIVER R63315_GWMD13752A_lcm_drv;
 
 #ifdef BUILD_LK
 extern void mdelay(unsigned long msec);
